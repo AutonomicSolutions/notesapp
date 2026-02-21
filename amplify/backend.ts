853a1b16b3a40aedfc23b4fc4dabf28f8feda980
Medhansh-Xplores <me23btech11046@iith.ac.in>
2026-02-21 09:04:11 +0000

    remove region

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,14 +2,11 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-
 /**
- * @see https://docs.amplify.aws/react/build-a-backend/
+ * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-
-export const backend = defineBackend({
+defineBackend({
   auth,
   data,
-  storage,
-  region: 'us-east-1', 
+  storage
 });
